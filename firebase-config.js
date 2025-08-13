3e8d457d5856e5ae8da2cb5a6aa35a10f0f35c12
movin980 <mgichimu@kabarak.ac.ke>
2025-08-13 17:30:26 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,16 @@
 // firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCozdGGtbY5VQeDxSs8Vo5IqX5nqTAEVcw",
-  authDomain: "ai-for-small-plot-landlords.firebaseapp.com",
-  projectId: "ai-for-small-plot-landlords",
-  storageBucket: "ai-for-small-plot-landlords.appspot.com",
-  messagingSenderId: "657855602798",
-  appId: "1:657855602798:web:3009f2e383ce67e54fc67f",
-  measurementId: "G-D2HJ54T08M"
+  apiKey: "AIzaSyBqLfsIMLig9JN-WvQZ51kM6xmp38gl-Y8",
+  authDomain: "landlady-63b0b.firebaseapp.com",
+  projectId: "landlady-63b0b",
+  storageBucket: "landlady-63b0b.firebasestorage.app",
+  messagingSenderId: "596849789967",
+  appId: "1:596849789967:web:070e8b0d48e4088b6d677a",
+  measurementId: "G-8YKC8R3VRR"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
